e12a4d948cdd986b974662186e4180bdd46b1818
Romch33tos <Romch33tos@yandex.ru>
2025-01-24 19:00:48 +0700

    Произведение и сумма
    Задача 1

---
+++ main.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    setlocale(LC_ALL, "russian");
+    const int size = 5;
+    int numbers[size];
+    int sum = 0;
+    int product = 1;
+    cout << "Введите " << size << " чисел:" << endl;
+    for (int i = 0; i < size; i++) {
+        cout << "Число " << (i + 1) << ": ";
+        cin >> numbers[i];
+    }
+    for (int i = 0; i < size; i++) {
+        sum += numbers[i];
+        product *= numbers[i];
+    }
+    cout << "Сумма чисел: " << sum << endl;
+    cout << "Произведение чисел: " << product << endl;
+    return 0;
+}
