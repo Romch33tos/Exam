d9fc4d23d755abffe375238eedd68caf103adae0
Romch33tos <Romch33tos@yandex.ru>
2025-01-24 19:22:12 +0700

    Произведение и сумма
    Задача 1

--- main.cpp
+++ main.cpp
@@ -3,24 +3,22 @@ using namespace std;
 
 int main() {
     setlocale(LC_ALL, "russian");
-    int sizeOfMassive = 5;
-    cout << "Введите размер массива: ";
-    cin >> sizeOfMassive;
-    int numbers[sizeOfMassive];
-    cout << "Введите 5 чисел:" << endl;
-    for (int i = 0; i < sizeOfMassive; i++) {
-        cout << "Число " << (i + 1) << ": ";
+    int size;
+    cout << "Введите размер массива:";
+    cin >> size;
+    int numbers[size];
+    int sum = 0;
+    int product = 1;
+    cout << "Введите " << size << "чисел :" << endl;
+    for (int i = 0; i < size; i++) {
+        cout << "×èñëî " << (i + 1) << ": ";
         cin >> numbers[i];
     }
-
-    int maximum = numbers[0];
-    for (int i = 1; i < sizeOfMassive; i++) {
-        if (numbers[i] > maximum) {
-            maximum = numbers[i];
-        }
+    for (int i = 0; i < size; i++) {
+        sum += numbers[i];
+        product *= numbers[i];
     }
-
-    cout << "Максимальное число: " << maximum << endl;
-
+    cout << "Сумма чисел: " << sum << endl;
+    cout << "Произведение чисел " << product << endl;
     return 0;
 }
